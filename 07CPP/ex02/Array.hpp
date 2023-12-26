dbf38d0122b5485f506849b9f55f44b0feee79f0
LemonMints42BKK <65420071@kmitl.ac.th>
2023-12-26 22:56:57 +0700

    CPP07 ex02

--- 07CPP/ex02/Array.hpp
+++ 07CPP/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: pnopjira <65420071@kmitl.ac.th>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/25 14:29:52 by pnopjira          #+#    #+#             */
-/*   Updated: 2023/12/25 20:45:42 by pnopjira         ###   ########.fr       */
+/*   Updated: 2023/12/26 22:55:20 by pnopjira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,43 @@
 
 #include <iostream>
 #include <iomanip>
+#include <cstdlib>
 
 template <typename T>
 class Array {
-	private:
-		Array<T>(void) {}
-		Array<T>(unsigned int n) { T *this->_arr = new int(n); std::cout << *this->_arr << std::endl;}
-		Arrey<T>(Array const &src);
-		~Array<T>(void) {} 
+	
+	class IndexOutOfLimit : public std::exception
+	{
+		public:
+			virtual const char* what() const throw() {return ("Index number is out of limit");}
+	};
 	
 	public:
+		Array<T>(void) {}
+		Array<T>(unsigned int n): _arr(new T[n]), _n(n){
+			for (unsigned int i = 0; i < n; i++)
+				this->_arr[i] = 0;}
+		Array<T>(Array const &src): _arr(new T[src._n]),  _n(src._n){
+			for (unsigned int i = 0; i < src._n; i++)
+				this->_arr[i] = src._arr[i];}
+		Array & operator=(Array const &rhs){
+			if (this != &rhs)
+			{
+				delete [] this->_arr;
+				this->_n = rhs._n;
+				this->_arr = new T[rhs._n];
+				for (unsigned int i = 0; i < rhs._n; i++)
+					this->_arr[i] = rhs._arr[i];
+			} else{return *this;}
+			return *this;
+		}
+		T & operator[](unsigned int const & i) const {if (i >= this->_n)throw IndexOutOfLimit(); return this->_arr[i];}
+		T & size(void) const {return (static_cast<T>(this->_n));}
+		~Array<T>(void) {delete [] this->_arr;}
+	
+	private:
 		T *	_arr;
-}
+		unsigned int _n;
+};
 
-
+#include "Array.tpp"
