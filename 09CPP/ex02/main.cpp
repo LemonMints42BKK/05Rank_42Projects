523d55c2728ee544041d404079f0f70737fff504
pnopjira <65420071@kmitl.ac.th>
2024-01-11 12:42:02 +0700

    cpp09 ex02

--- 09CPP/ex02/main.cpp
+++ 09CPP/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pnopjira <65420071@kmitl.ac.th>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 11:27:32 by pnopjira          #+#    #+#             */
-/*   Updated: 2024/01/10 23:39:10 by pnopjira         ###   ########.fr       */
+/*   Updated: 2024/01/11 12:22:15 by pnopjira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,24 @@ int main(int argc, char **argv){
 		for (int i = 1; i < argc; i++){
 			for(int j = 0; argv[i][j]; j++){
 				if (!isdigit(argv[i][j])){
-					throw std::invalid_argument("Invalid argument");
+					throw std::invalid_argument("Error");
 				}	
 			}
 			arg.push_back(argv[i]);
 		}
-		PmergeMe<std::vector<size_t> > pmm(arg);
+		PmergeMe<std::vector<size_t> > vec(arg);
+		PmergeMe<std::deque<size_t> > deq(arg);
+		std::cout << "Before: ";
+		vec.printSort();
 		
+		vec.mergeInsertSort(vec.getData());
+		deq.mergeInsertSort(deq.getData());
+		
+		std::cout << "After: ";
+		vec.printSort();
+		
+		std::cout << "Time to process a range of "<< vec ;
+		std::cout << "Time to process a range of "<< deq ;
 	} catch (std::exception &e){
 		std::cout << e.what() << std::endl;
 	}
